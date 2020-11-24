d8b3af514cbb0ff197eb8ecc9e7374ec50b39ee5
VladimirDeveloper <git.vladimir.developer@mail.ru>
2020-11-24 18:35:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-bmq3p3-rvvh5p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bmq3p3-rvvh5p)
---
+++ src/app/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    name: "Phone XL",
+    price: 799,
+    description: "A large phone with one of the best screens"
+  },
+  {
+    name: "Phone Mini",
+    price: 699,
+    description: "A great phone with one of the best cameras"
+  },
+  {
+    name: "Phone Standard",
+    price: 299,
+    description: ""
+  },
+  {
+    name: "Хонор",
+    price: 754,
+    description: "Прекрасный выбор в категрии цена/качество"
+  }
+];
